cb90e0056ea48c6442526550cc7e7051f4c68764
sergiolv <sergio.lagos@mail.udp.cl>
2022-11-27 18:26:31 -0300

    asdfas

---
+++ scripts/wp.py
@@ -0,0 +1,48 @@
+# import wikipedia as wiki
+
+# jdm = ["mazda", "nissan", "toyota","mitsubishi","suzuki"]
+# luxury = ["bentley", "ferrari", "lamborghini", "lotus", "bugatti"]
+
+# for brand in jdm:
+#     f = open(f"carpeta1/{brand}.txt","a")
+#     wp = wiki.page(brand)
+#     f.write(brand + "\n" + wp.content)
+
+# for brand in luxury:
+#     f = open(f"carpeta2/{brand}.txt","a")
+#     wp = wiki.page(brand)
+#     f.write(brand + "\n" + wp.content)
+import requests
+import json
+
+entradas = ["mazda", "nissan", "toyota","mitsubishi","suzuki", "bentley", "ferrari", "lamborghini", "lotus", "bugatti"]
+url = "https://en.wikipedia.org/w/api.php"
+i = 1
+for entrada in entradas:
+    params = {
+        'format': 'json',
+        'action': 'query',
+        'prop': 'extracts',
+        'exintro': '',
+        'explaintext': '',
+        'redirects': 1,
+        'titles': entrada
+    }
+
+    req = requests.get(
+        url,
+        params=params
+    ).json()
+
+    n_page = list(req['query']['pages'].keys())[0]
+    texto = req['query']['pages'][n_page]['extract']
+    texto = '{}<splittername>{}'.format(i, json.dumps(texto))
+
+    if i <= 5:
+        with open(f'carpeta1/{entrada}.txt', 'w') as f:
+            f.write(texto)
+    else:
+        with open(f'carpeta2/{entrada}.txt', 'w') as f:
+            f.write(texto)
+    i = i + 1
+
