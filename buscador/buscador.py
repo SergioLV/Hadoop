0866c93f556811f710a8fd162b0e6173dd00c23f
sergiolv <sergio.lagos@mail.udp.cl>
2022-11-27 19:10:23 -0300

    asdfasd

--- buscador/buscador.py
+++ buscador/buscador.py
@@ -3,18 +3,19 @@ import re
 
 dict = {
     "1":"https://es.wikipedia.org/wiki/mazda",
-    "2":"https://es.wikipedia.org/wiki/mitsubishi",
-    "3":"https://es.wikipedia.org/wiki/nissan",
-    "4":"https://es.wikipedia.org/wiki/suzuki",
-    "5":"https://es.wikipedia.org/wiki/toyota",
+    "2":"https://es.wikipedia.org/wiki/nissan",
+    "3":"https://es.wikipedia.org/wiki/toyota",
+    "4":"https://es.wikipedia.org/wiki/mitsubishi",
+    "5":"https://es.wikipedia.org/wiki/suzuki",
     "6":"https://es.wikipedia.org/wiki/bentley",
-    "7":"https://es.wikipedia.org/wiki/bugatti",
-    "8":"https://es.wikipedia.org/wiki/ferrari",
-    "9":"https://es.wikipedia.org/wiki/lamborghini",
-    "10":"https://es.wikipedia.org/wiki/lotus",
+    "7":"https://es.wikipedia.org/wiki/ferrari",
+    "8":"https://es.wikipedia.org/wiki/lamborghini",
+    "9":"https://es.wikipedia.org/wiki/lotus",
+    "10":"https://es.wikipedia.org/wiki/bugatti",
 }
 for line in sys.stdin:
     if(line.split()[0] == sys.argv[1]):
+        # obtener ls
         aux=re.findall('\[[^\]]*\]|\([^\)]*\)|\"[^\"]*\"|\S+',line)
         max = 0
         res = 0
@@ -22,6 +23,9 @@ for line in sys.stdin:
             if eval(tuple)[1] > max:
                 max = eval(tuple)[1]
                 res = eval(tuple)[0]
-
-print(dict[str(res)])
-print(sys.argv[1])
+try:    
+    print(res)
+    print(dict[str(res)])
+    print(sys.argv[1])
+except:
+    print("palabra no encontrada!")
